138f06bb33ab6e202bedf020724575b684b9b10e
Tian Zhang <tian@kv.seastar.sandstorm.emulab.net>
2020-02-03 02:55:31 -0700

    Add compile script.

---
+++ compile_v8_seastar.sh
@@ -0,0 +1,8 @@
+./setup_v8.sh
+git submodule update --init --recursive
+sudo apt-get install -y build-essential linux-image-extra-virtual
+cd seastar
+./install-dependencies.sh
+./configure.py --enable-dpdk --mode=release --c-compiler=gcc-5
+ninja
+cd ..

784dfade26fbb47f235ce3675eda097d2a6128d8
Tian Zhang <tian@kv.seastar.sandstorm.emulab.net>
2020-02-04 23:22:52 -0700

    Modify seastar setup script.

--- setup_seastar.sh
+++ setup_seastar.sh
@@ -1,7 +1,7 @@
 git submodule update --init --recursive
 sudo apt-get install -y build-essential linux-image-extra-virtual
 cd seastar
-./install-dependencies.sh
+sudo ./install-dependencies.sh
 ./configure.py --enable-dpdk --mode=release --c-compiler=gcc-5
 ninja
 cd ..
